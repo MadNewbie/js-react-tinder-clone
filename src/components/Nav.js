4e6b781e921fc0766a93827f338f5af819648d1e
MadNewbie <rachmad.y25@gmail.com>
2022-03-19 10:46:13 +0700

    make auth modal UI

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,23 @@
 import whiteLogo from '../images/white-logo-tinder.png';
 import colorLogo from '../images/color-logo-tinder.png'
-const Nav = ({minimal, authToken}) => {
+const Nav = ({minimal, authToken, setShowModal, showModal, setIsSignUp}) => {
+    
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
+    
     return (
         <nav>
             <div className="logo-container">
                 <img className="logo" src={minimal ? colorLogo : whiteLogo}/>
             </div>
 
-            {!authToken && !minimal && <button className='nav-button'>Log in</button>}
+            {!authToken && !minimal && <button 
+                className='nav-button'
+                onClick={handleClick}
+                disabled={showModal}
+            >Log in</button>}
         </nav>
     )
 }
